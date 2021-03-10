910782109cb33fd597bb5746012338dc2828ca84
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2021-03-09 21:41:13 -0600

    Add files via upload

---
+++ Laboratorio-15/crear.sql
@@ -0,0 +1,32 @@
+/*Tabla de Materiales*/
+CREATE TABLE Materiales
+(
+  Clave numeric(5),
+  Descripcion varchar(50),
+  Costo numeric(8,2)
+)
+
+/*Tabla de Proveedores*/
+CREATE TABLE Proveedores
+(
+  RFC char(13),
+  RazonSocial varchar(50),
+)
+
+/*Tabla de Proyectos*/
+CREATE TABLE Proyectos
+(
+  Numero numeric(5),
+  Denominacion varchar(50),
+)
+
+
+/*Tabla de Entregan*/
+CREATE TABLE Entregan
+(
+  Clave numeric(5),
+  RFC char(13),
+  Numero numeric(5),
+  Fecha datetime,
+  Costo numeric(8,2)
+)
