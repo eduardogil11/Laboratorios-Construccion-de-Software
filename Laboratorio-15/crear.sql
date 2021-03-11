64c96d9e24076b3852c4378fdc2ad40910f1e068
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2021-03-11 16:58:11 -0600

    Laboratorio 15

--- Laboratorio-15/crear.sql
+++ Laboratorio-15/crear.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*Tabla de Materiales*/
 CREATE TABLE Materiales
 (
@@ -36,43 +35,4 @@ CREATE TABLE Entregan
 drop table Materiales, Proveedores, Proyectos, Entregan
 
 /*Consultar tablas existentes*/
-=======
-/*Tabla de Materiales*/
-CREATE TABLE Materiales
-(
-  Clave numeric(5),
-  Descripcion varchar(50),
-  Costo numeric(8,2)
-)
-
-/*Tabla de Proveedores*/
-CREATE TABLE Proveedores
-(
-  RFC char(13),
-  RazonSocial varchar(50),
-)
-
-/*Tabla de Proyectos*/
-CREATE TABLE Proyectos
-(
-  Numero numeric(5),
-  Denominacion varchar(50),
-)
-
-
-/*Tabla de Entregan*/
-CREATE TABLE Entregan
-(
-  Clave numeric(5),
-  RFC char(13),
-  Numero numeric(5),
-  Fecha datetime,
-  Costo numeric(8,2)
-)
-
-/*Borrar tablas*/
-drop table Materiales, Proveedores, Proyectos, Entregan
-
-/*Consultar tablas existentes*/
->>>>>>> 6be1e3b062fdb9ee94658c6136bfbbc8e992530c
 select * from sysobjects where xtype='U'
